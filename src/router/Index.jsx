5ae5272366ee5d76b3889df4c87a365bf97e818d
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-13 12:17:02 +0800

    update route

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Home, Splash , Akun , Pesanan} from '../page/Index';
+import { Home, Splash, Akun, Pesanan } from '../page/Index';
+import { BottomNavigation } from '../components/Index';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <BottomNavigation {...props} ></BottomNavigation>}>
       <Tab.Screen name='Home' component={Home} options={{ headerShown: false }}></Tab.Screen>
       <Tab.Screen name='Pesanan' component={Pesanan} options={{ headerShown: false }}></Tab.Screen>
       <Tab.Screen name='Akun' component={Akun} options={{ headerShown: false }}></Tab.Screen>
