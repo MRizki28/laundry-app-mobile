40aed24eb0f7f1cfa7501d674264a65f52c2a7d6
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-13 12:16:07 +0800

    exprt asset icon

---
+++ src/assets/icons/Index.jsx
@@ -0,0 +1,8 @@
+import IconAkun from './akun.svg'
+import IconAkunActive from './akun-active.svg'
+import IconPesanan from './pesanan.svg'
+import IconPesananActive from './pesanan-active.svg'
+import IconHome from './home.svg'
+import IconHomeActive from './home-active.svg'
+
+export { IconAkun, IconAkunActive, IconPesanan, IconPesananActive, IconHome, IconHomeActive }
